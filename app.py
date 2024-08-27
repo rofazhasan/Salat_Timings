a4fa9f07e29ccf41ed7ccbf93299e4abe42c5479
Rofaz <mdrofazhasanrafiu@gmail.com>
2024-08-27 17:38:57 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request
+from salat import get_salat_timings
+from waitress import serve
+from datetime import datetime
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template('index.html')
+@app.route('/salat')
+def salat():
+  city=request.args.get('city')
+  country=request.args.get('country')
+  day_a=request.args.get('date')
+
+
+  if not bool(city.strip()):
+    city="Tangail"
+  if not bool(country.strip()):
+    country = "Bangladesh"
+  if not bool(day_a.strip()):
+    day=datetime.now().strftime('%d-%m-%Y')
+  else:
+    day = datetime.strptime(day_a, '%Y-%m-%d').strftime('%d-%m-%Y')
+
+  get_n=get_salat_timings(day,country,city)
+  if  get_n==201:
+    return render_template('not_found.html')
+
+  return render_template('salat.html',city=city,country=country,date=get_n[5],
+                         fajr=get_n[0],
+                         dhuhr=get_n[1],
+                         asr=get_n[2],
+                         maghrib=get_n[3],
+                         isha=get_n[4]
+                         )
+
+
+if __name__ == '__main__':
+  serve(app,host="0.0.0.0",port=8080)
